4104b16e52e9dd4e2d2825993e7336f6a986ce82
IrNesterova <gassae@gmail.com>
2019-02-21 06:57:28 +0300

    rps

--- rockpaperscissorssocket'/src/main/java/servers/GameServer.java
+++ rockpaperscissorssocket'/src/main/java/servers/GameServer.java
@@ -48,40 +48,66 @@ public class GameServer {
 
         public void run(){
             try{
-                if (clients.size()>=2) {
+                if (clients.size() % 2 == 0) {
                     System.out.println("Hello! This is RPS game, type R for rock, P for paper, S for scissors.");
 
-                    //in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    in_1 = new BufferedReader(new InputStreamReader(clients.get(0).clientSocket.getInputStream()));
-                    in_2 = new BufferedReader(new InputStreamReader(clients.get(1).clientSocket.getInputStream()));
+                        for (int i = 0; i < clients.size(); i = i+2){
+                            in_1 = new BufferedReader(new InputStreamReader(clients.get(i).clientSocket.getInputStream()));
+                            in_2 = new BufferedReader(new InputStreamReader(clients.get(i+1).clientSocket.getInputStream()));
+                            String inputLine_1;
+                            String inputLine_2;
+                            while ((inputLine_1 = in_1.readLine()) != null && (inputLine_2 = in_2.readLine()) != null) {
+                                if(inputLine_1.equals(inputLine_2)){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("draw");
+                                    }
+                                } else if(inputLine_1.equals("R") && inputLine_2.equals("S")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("first player wins");
+                                    }
 
-                    String inputLine;
-                    String inputLine_1;
-                    String inputLine_2;
-                    while ((inputLine_1 = in_1.readLine()) != null && (inputLine_2 = in_2.readLine()) != null) {
-                        if (".".equals(inputLine_1) && ".".equals(inputLine_2)) {
-                            for (ClientHandler client : clients) {
-                                PrintWriter out = new PrintWriter(client.clientSocket.getOutputStream(), true);
-                                out.println("bye");
-                            }
-                            break;
-                        }  else if (("R".equals(inputLine_1) && "S".equals(inputLine_2)) || ("S".equals(inputLine_1) && "P".equals(inputLine_2))
-                                || ("P".equals(inputLine_1) && "R".equals(inputLine_2))) {
-                            for (ClientHandler client : clients) {
-                                PrintWriter out = new PrintWriter(client.clientSocket.getOutputStream(), true);
-                                out.print("Player" + clients.get(0).clientSocket.getPort() + "won");
-                            }
-                        } else {
-                            for (ClientHandler clientHandler : clients) {
-                                PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
-                                out.print("Player" + clients.get(1).clientSocket.getPort() + "won");
-                            }
+                                } else if (inputLine_1.equals("S") && inputLine_2.equals("R")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("second player wins");
+                                    }
+
+                                } else if (inputLine_1.equals("R") && inputLine_2.equals("P")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("second player wins");
+                                    }
+
+                                } else if (inputLine_1.equals("P") && inputLine_2.equals("R")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("player one wins");
+                                    }
+
+                                } else if (inputLine_1.equals("S") && inputLine_2.equals("P")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("player one wins");
+                                    }
+                                    System.out.println("Player one wins");
+                                } else if (inputLine_1.equals("P") && inputLine_2.equals("S")){
+                                    for (ClientHandler clientHandler : clients){
+                                        PrintWriter out = new PrintWriter(clientHandler.clientSocket.getOutputStream(), true);
+                                        out.println("player two");
+                                    }
+                                } else if(inputLine_1.equals(".") && inputLine_2.equals(".")){
+                                    break;
+                                }
                         }
+
+                        in_1.close();
+                        in_2.close();
+                        clientSocket.close();
                     }
-                    in_1.close();
-                    in_2.close();
-                    clientSocket.close();
                 }
+
 
             } catch (Exception e){
                 throw new IllegalStateException(e);
